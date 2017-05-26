0c69f7d6af4e1d2b681a4f6de448b44858eee847
AntonShumin <AntonShuminBe@gmail.com>
2017-05-26 23:24:01 +0200

    velocity based haptic feedback

--- Assets/Operational/Script/script_grabbable.cs
+++ Assets/Operational/Script/script_grabbable.cs
@@ -9,10 +9,14 @@
 
         private GameObject grabbingController;
 
+        //cashed 
+        private float impact_velocity;
+
         public override void Grabbed(GameObject grabbingObject)
         {
             base.Grabbed(grabbingObject);
             grabbingController = grabbingObject;
+            Vibrate(0.2f, 0.1f);
         }
 
         public override void Ungrabbed(GameObject previousGrabbingObject)
@@ -24,13 +28,25 @@
 
         private void OnCollisionEnter(Collision collision)
         {
+
+            //set vars
+            impact_velocity = 1f / 40f * collision.relativeVelocity.magnitude;
+            impact_velocity = Mathf.Min(30f,impact_velocity);
+            impact_velocity = Mathf.Max(0.05f,impact_velocity);
+            Debug.Log("haptic strength " + impact_velocity);
+
+            //haptic feedback
             if (grabbingController != null && IsGrabbed())
             {
 
-                var hapticStrength = 1;
-                VRTK_SharedMethods.TriggerHapticPulse(VRTK_DeviceFinder.GetControllerIndex(grabbingController), hapticStrength, 0.5f, 0.01f);
+                Vibrate(impact_velocity, 0.1f);
             }
             
+        }
+
+        private void Vibrate(float strength, float duration)
+        {
+            VRTK_SharedMethods.TriggerHapticPulse(VRTK_DeviceFinder.GetControllerIndex(grabbingController), strength, duration, 0.01f);
         }
 
         
