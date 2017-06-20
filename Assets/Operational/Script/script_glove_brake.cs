a4d44777adb8bddb5e2596c911dab01e222d6ec2
AntonShumin <AntonShuminBe@gmail.com>
2017-06-20 23:08:42 +0200

    glove colliders and triggers

---
+++ Assets/Operational/Script/script_glove_brake.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class script_glove_brake : MonoBehaviour {
+
+	void OnTriggerEnter(Collider collider)
+    {
+        if (collider.GetComponent<script_puck>())
+        {
+            collider.GetComponent<Rigidbody>().velocity = Vector3.zero;
+        }
+    }
+}
