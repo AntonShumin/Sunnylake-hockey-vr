179426f4d42eac02c95e3b81cc789d0486128515
AntonShumin <AntonShuminBe@gmail.com>
2017-07-09 23:30:00 +0200

    v2 stick-ice offset script

--- Assets/Operational/Script/Equipment/script_goalie_pad.cs
+++ Assets/Operational/Script/Equipment/script_goalie_pad.cs
@@ -5,9 +5,11 @@ using UnityEngine;
 public class script_goalie_pad : MonoBehaviour {
 
     //game objects, transforms and rigidbodies
-    public Transform m_ground_detector;
-    public Transform m_stick;
+    private Transform m_pad_reference_ground;
+    private Transform m_pad_reference_stick;
+    private Transform m_stick;
     private Rigidbody m_Rigidbody;
+    private Transform m_Ground;
 
     //positions
     public Vector3 m_stick_original_position;
@@ -23,32 +25,73 @@ public class script_goalie_pad : MonoBehaviour {
     private float angularVelocityLimit = float.PositiveInfinity;
     private float velocityLimit = float.PositiveInfinity;
 
+    //cached calculation vars for the stick movement
+    private Vector3 c_p1;
+    private Vector3 c_p0;
+    private Vector3 c_pg;
+    private float c_k;
+    private Vector3 c_stick_offset;
+
 
     void Awake()
     {
-        m_ground_detector = transform.FindChild("Goalie_Pad_Hand/stick_ground_detector");
-        m_stick = transform.FindChild("model_goalie_stick_v1");
+        m_pad_reference_ground = GameObject.Find("stick_ground_detector").transform; //p0
+        m_pad_reference_stick = GameObject.Find("stick_upper_detector").transform; //p1
+        m_stick = GameObject.Find("model_goalie_stick_v1").transform;
         m_stick_original_position = m_stick.position;
         m_Rigidbody = m_stick.GetComponent<Rigidbody>();
+
+
+    }
+
+    void Start()
+    {
+        m_Ground = GameObject.Find("ice_plane").transform;
+
+        //cached
+        c_pg = m_Ground.position;
+        c_stick_offset = m_stick.position - m_pad_reference_ground.position;
+
     }
 
 	void FixedUpdate()
     {
-        Debug.Log(m_stick.position);
-        //Move_Stick();
+        //Debug.Log(m_stick.position);
+        Move_Stick();
     }
+
+    
 
     private void Move_Stick()
     {
 
         //calculate new position
-        m_position_reference = Vector3.zero;
-        m_rotation_reference = Quaternion.identity;
+
+        //if below ground
+        c_p1 = m_pad_reference_stick.position;
+        c_p0 = m_pad_reference_ground.position;
+        c_pg = m_Ground.position;
+        if ( c_p0.y < c_pg.y )
+        {
+            c_k = (c_pg.y - c_p1.y) / (c_p0.y - c_p1.y);
+            c_pg.x = c_p1.x + c_k * (c_p0.x - c_p1.x);
+            c_pg.z = c_p1.z + c_k * (c_p0.z - c_p1.z);
 
         
+        //else set stick height to reference height        
+        } else
+        {
+            c_pg = c_p0;
+
+        }
+
+        //define new coordinates
+        m_position_reference = c_pg + c_stick_offset;
+        m_rotation_reference = m_pad_reference_stick.rotation;
 
 
-        //VRTK movement
+
+        //VRTK - move the stick
         rotationDelta = m_rotation_reference * Quaternion.Inverse(transform.rotation);
         positionDelta = m_position_reference - transform.position;
 
