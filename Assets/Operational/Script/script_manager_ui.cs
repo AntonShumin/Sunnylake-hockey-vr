53bc20754cfdd371ea6c3b6599f259d339033ce2
AntonShumin <AntonShuminBe@gmail.com>
2017-07-31 19:34:40 +0200

    preset challenges and currency #22

--- Assets/Operational/Script/script_manager_ui.cs
+++ Assets/Operational/Script/script_manager_ui.cs
@@ -13,12 +13,16 @@ public class script_manager_ui : MonoBehaviour {
     private GameObject m_ui_main_goalie;
     private List<GameObject> m_uit_main_goalie_children = new List<GameObject>();
 
+    private GameObject m_ui_main_challenges;
+    private List<GameObject> m_uit_main_challenges_children = new List<GameObject>();
+
     void Awake()
     {
 
         m_ui_main = GameObject.Find("UI_Main").gameObject;
         m_ui_main_goalie = GameObject.Find("UI_Main_Goalie").gameObject;
-        
+        m_ui_main_challenges = GameObject.Find("UI_Main_Challenges").gameObject;
+
         m_manager_gameplay = GameObject.Find("Manager_Gameplay").GetComponent<script_manager_gameplay>();
         m_manager_ui_world = GameObject.Find("World Canvas").GetComponent<script_manager_ui_world>();
     }
@@ -38,6 +42,15 @@ public class script_manager_ui : MonoBehaviour {
             m_uit_main_goalie_children.Add(child.gameObject);
             child.gameObject.SetActive(false);
         }
+
+        //challenges list
+        foreach (Transform child in m_ui_main_challenges.transform)
+        {
+            m_uit_main_challenges_children.Add(child.gameObject);
+            child.gameObject.SetActive(false);
+        }
+
+        //other
         gameObject.SetActive(false);
 
     }
@@ -53,16 +66,23 @@ public class script_manager_ui : MonoBehaviour {
         switch (id)
         {
             case "goalie":
-                foreach( GameObject child in m_ui_main_children)                {                    child.SetActive(false);               }
-                foreach (GameObject child in m_uit_main_goalie_children)        {                    child.SetActive(true);                }
+                foreach( GameObject child in m_ui_main_children)             {  child.SetActive(false);  }
+                foreach (GameObject child in m_uit_main_goalie_children)     {  child.SetActive(true);   }
+                foreach (GameObject child in m_uit_main_challenges_children) { child.SetActive(false);   }
                 break;
             case "to_main":
-                foreach (GameObject child in m_ui_main_children)                {                    child.SetActive(true);                }
-                foreach (GameObject child in m_uit_main_goalie_children)        {                    child.SetActive(false);               }
+                foreach (GameObject child in m_ui_main_children)             {  child.SetActive(true);   }
+                foreach (GameObject child in m_uit_main_goalie_children)     {  child.SetActive(false);  }
+                foreach (GameObject child in m_uit_main_challenges_children) { child.SetActive(false);   }
                 break;
             case "cannon":
                 m_manager_ui_world.Hide_main_ui();
                 m_manager_gameplay.Game_Event(id);
+                break;
+            case "challenges":
+                foreach (GameObject child in m_ui_main_children) { child.SetActive(false); }
+                foreach (GameObject child in m_uit_main_goalie_children) { child.SetActive(false); }
+                foreach (GameObject child in m_uit_main_challenges_children) { child.SetActive(true); }
                 break;
         }
     }
---
+++ Assets/Operational/Script/script_memory_bank.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+
+public class script_memory_bank : MonoBehaviour {
+
+    private string m_version;
+    private string m_version_current = "0.1"; //in case of a mismatch, clear everything
+    private int m_pucks;
+    private string m_username;
+    private string m_password;
+
+    //accessors 
+    public int Pucks
+    {
+        get { return m_pucks; }
+    }
+
+    void Awake()
+    {
+        Load_Data();
+    }
+
+    private void Load_Data()
+    {
+        //version check
+        if( ES2.Exists("version") )
+        {
+
+            m_version = ES2.Load<string>("versions");
+            if (m_version_current != m_version) Reset_All();
+
+        } else
+        {
+
+            m_version = m_version_current;
+
+        }
+
+        //pucks
+        if (ES2.Exists("pucks"))
+        {
+            m_pucks = ES2.Load<int>("pucks");
+        } else
+        {
+            m_pucks = 0;
+        }
+    }
+
+    
+    public void Add_Pucks(int value)
+    {
+
+        m_pucks += value;
+        ES2.Save(m_pucks, "pucks");
+
+    }
+
+    private void Reset_All()
+    {
+        //implementation
+    }
+
+
+}
