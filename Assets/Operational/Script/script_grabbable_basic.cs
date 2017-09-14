c94ca16891150c6b06b995209102fccc9b0df71e
AntonShumin <AntonShuminBe@gmail.com>
2017-09-14 20:53:44 +0200

    rebuilding grab attach

---
+++ Assets/Operational/Script/script_grabbable_basic.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+
+public class script_grabbable_basic : VRTK_InteractableObject
+{
+
+    private GameObject grabbingController;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+
+    public override void Grabbed(GameObject grabbingObject)
+    {
+        base.Grabbed(grabbingObject);
+        grabbingController = grabbingObject;
+        Vibrate(0.2f, 0.1f);
+    }
+
+    public override void Ungrabbed(GameObject previousGrabbingObject)
+    {
+        base.Ungrabbed(previousGrabbingObject);
+        grabbingController = null;
+    }
+
+    public void Vibrate(float strength, float duration)
+    {
+        if (grabbingController != null && IsGrabbed())
+        {
+            VRTK_SharedMethods.TriggerHapticPulse(VRTK_DeviceFinder.GetControllerIndex(grabbingController), strength, duration, 0.01f);
+        }
+
+    }
+
+
+    public void Vibrate_pulse(float strength)
+    {
+        if (grabbingController != null && IsGrabbed())
+        {
+            VRTK_SharedMethods.TriggerHapticPulse(VRTK_DeviceFinder.GetControllerIndex(grabbingController), strength);
+        }
+    }
+
+}
